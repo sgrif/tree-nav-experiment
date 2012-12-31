e02080ecc26076091d0c48d10126c037f9d6e8d4
Sean Griffin <sean.griffin@boomtime.com>
2012-12-31 13:41:54 -0700

    Handle parent deletion
    Forward porting adopt orphan_strategy

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,18 @@
 class Page < ActiveRecord::Base
   attr_accessible :parent_id, :content, :name
 
-  has_ancestry
+  before_destroy :move_orphans
+
+  has_ancestry orphan_strategy: :restrict
+
+  def move_orphans
+    unless self.class.orphan_strategy == :adopt
+      descendants.all.each do |descendant|
+        descendant.without_ancestry_callbacks do
+          new_ancestry = descendant.ancestor_ids.delete_if { |x| x == self.id }.join("/")
+          descendant.update_attribute descendant.class.ancestry_column, new_ancestry || nil
+        end
+      end
+    end
+  end
 end
