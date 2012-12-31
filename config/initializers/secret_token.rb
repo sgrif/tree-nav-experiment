082b879e52e9e6d8db1b76551d35b9f24ad19034
Sean Griffin <sean.griffin@boomtime.com>
2012-12-31 12:45:19 -0700

    Working navigation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TreeNavExperiment::Application.config.secret_token = '042e33f11ca1ee2c017a039465e1226e15e699e1cb12b7bd5e2baa82e1fec7efa06b0aa4358dfb0690fa5a4c0543f8edde8e4ca7050d853b78374360c8de71f6'
